a453dafcf80b3a04bb165a673095ab012f9b2d6b
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2023-12-23 21:57:04 +0700

    develop product section

--- components/ServiceCarousel.js
+++ components/ServiceCarousel.js
@@ -2,17 +2,20 @@ import React from 'react'
 import Image from 'next/image'
 
 import s1 from '/assets/images/s1.png'
+import s2 from '/assets/images/s2.png'
+import s3 from '/assets/images/s3.png'
 import s4 from '/assets/images/s4.png'
+import s5 from '/assets/images/s5.png'
 import s6 from '/assets/images/s6.png'
 
 export default function ServiceCarousel() {
   const services = [
-    { image: s1, title: "Home Welding", description: "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal" },
-    { image: s4, title: "Machine Welding", description: "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal" },
-    { image: s6, title: "Car Welding", description: "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal" },
-    { image: s1, title: "Home Welding", description: "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal" },
-    { image: s4, title: "Machine Welding", description: "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal" },
-    { image: s6, title: "Car Welding", description: "when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal" },
+    { image: s1, title: "Moringa Leaf Powder", description: " Moringa leaf powder is rich in vitamins, minerals, and amino acids. It can be used in a variety of ways such as in smoothies, soups, and tea." },
+    { image: s2, title: "Moringa Tea", description: "Made from dried Moringa leaves. It is a refreshing beverage that is also packed with antioxidants and anti-inflammatory compounds." },
+    { image: s3, title: "Moringa Oil", description: "Derived from the seeds of the Moringa tree. It's often used in skincare products due to its moisturizing and anti-aging properties." },
+    { image: s4, title: "Moringa Seeds", description: "Numerous applications in food, cosmetics, and pharmaceutical industries due to their antimicrobial and antioxidant properties." },
+    { image: s5, title: "Moringa Capsules", description: "Essentially moringa powder encapsulated for easy consumption. They are an excellent option for those who want to enjoy the health benefits of Moringa in a convenient, easy-to-consume form." },
+    { image: s6, title: "Moringa Flower", description: "Another part of the Moringa tree that's edible and has health benefits. In some cultures, they're used in cooking for their unique flavor." },
   ]
 
   return (
