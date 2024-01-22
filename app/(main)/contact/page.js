5d5ae9c2faf61732f4cb184734ab2da2c7113b10
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2024-01-22 20:09:18 +0700

    add location details

--- app/(main)/contact/page.js
+++ app/(main)/contact/page.js
@@ -13,13 +13,13 @@ export default function ContactUsPage() {
       <h1>Our Store Location</h1>
       <div className="map-responsive">
         {/* Replace the src attribute with the link to your Google Maps location */}
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50825.47862866222!2d111.85185208354169!3d-7.165299786089634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e77813aaa4b03a5%3A0x1a40bef6dbccb9ce!2sAlun%20-%20Alun%20Bojonegoro!5e0!3m2!1sen!2sid!4v1705433254456!5m2!1sen!2sid" width="1200" height="450" style={{ border:0, width:'100%' }} loading="lazy" referrerPolicy="no-referrer-when-downgrade" ></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.572107420103!2d111.61987728419624!3d-7.175358474341032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e776305871ac81d%3A0xea37c0d4854a321c!2sJl.%20Waduk%20Sonorejo%2C%20Kec.%20Padangan%2C%20Kabupaten%20Bojonegoro%2C%20Jawa%20Timur%2062162!5e0!3m2!1sen!2sid!4v1705923395714!5m2!1sen!2sid" width="1200" height="450" style={{ border:0, width:'100%' }} loading="lazy" referrerPolicy="no-referrer-when-downgrade" ></iframe>
       </div>
       <div className="address-details" style={{ marginTop:30 }}>
         {/* Replace with your store's address details */}
-        <p>1234 Street Name, City, Country</p>
-        <p>Open Hours: 9am - 5pm, Mon - Fri</p>
-        <p>Phone: (123) 456-7890</p>
+        <p>Jl. Waduk Sonorejo, Dusun Budengan </p>
+        <p>RT. 08 Rw. 03 Kec. Padangan</p>
+        <p>Kabupaten Bojonegoro, Jawa Timur, Indonesia</p>
       </div>
     </div>
   );
