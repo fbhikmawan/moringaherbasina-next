b0625ecf95c968bb73a8d31e3c34a4ebc9658e8c
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2024-02-11 06:51:26 +0700

    revert Suspense method, try dynamic method

--- app/page.js
+++ app/page.js
@@ -1,25 +1,24 @@
-import { Suspense } from 'react'
+import dynamic from 'next/dynamic'
 
-import HeaderSection from '/sections/HeaderSection'
-import SliderSection from '/sections/SliderSection'
 import ProductSection from '/sections/ProductSection'
 import AboutSection from '/sections/AboutSection'
 import AchievementSection from '/sections/AchievementSection'
-import TestimonialSection from '/sections/TestimonialSection'
 import BackToTopButton from '/components/ButtonBackToTop'
 
 export const metadata = {
   description: 'Moringa Herbasina. We produce, manage and deliver Moringa products for local and export markets. We are committed to deliver the best quality Moringa products to our customers.',
 }
+const TestimonialSection = dynamic(() => import('/sections/TestimonialSection'))
+const HeaderSection = dynamic(() => import('/sections/HeaderSection'))
+const SliderSection = dynamic(() => import('/sections/SliderSection'))
 
 export default function TopPage() {
+
   return (
     <>
       <div id="hero" className="hero_area">
         <HeaderSection isTopPage={true} />
-        <Suspense>
-          <SliderSection />
-        </Suspense>
+        <SliderSection />
       </div>
       <ProductSection layoutPadding={true} isTopPage={true} />
       <AboutSection isTopPage={true} layoutPadding={true} />
