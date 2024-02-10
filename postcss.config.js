631cea772315e3b9c7b4d5c2cb1871ff8ce83170
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2024-02-11 06:20:02 +0700

    Revert "try to add postcss"
    This reverts commit 1d17c42d66d71aac995a34e36a1a863c1a9b18a4.

--- postcss.config.js
+++ postcss.config.js
@@ -1,36 +1,6 @@
-// postcss.config.js
 module.exports = {
-  plugins: [
-    'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
-      },
-    ],
-    // Conditionally add PurgeCSS only for production builds
-    process.env.NODE_ENV === 'production'
-      ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            // Specify the paths to all of the template files in your project
-            content: [
-              './app/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}',
-              './sections/**/*.{js,jsx,ts,tsx}',
-            ],
-            // Include any special characters you're using in your CSS class names
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-            // Make sure CSS from external libraries is preserved
-            safelist: ['your', 'specific', 'classes'],
-          },
-        ]
-      : undefined,
-  ].filter(Boolean),
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
