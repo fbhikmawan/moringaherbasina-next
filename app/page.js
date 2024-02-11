6ec55d00d37c50320222de5faed54d406fff120e
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2024-02-11 07:50:01 +0700

    try to use lazy on Suspense

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import dynamic from 'next/dynamic'
+import { Suspense, lazy } from 'react';
 
 import ProductSection from '/sections/ProductSection'
 import AboutSection from '/sections/AboutSection'
@@ -8,22 +8,28 @@ import BackToTopButton from '/components/ButtonBackToTop'
 export const metadata = {
   description: 'Moringa Herbasina. We produce, manage and deliver Moringa products for local and export markets. We are committed to deliver the best quality Moringa products to our customers.',
 }
-const TestimonialSection = dynamic(() => import('/sections/TestimonialSection'), { ssr: false })
-const HeaderSection = dynamic(() => import('/sections/HeaderSection'), { ssr: false })
-const SliderSection = dynamic(() => import('/sections/SliderSection'), { ssr: false })
+const SliderSection = lazy(() => import('/sections/SliderSection'));
+const TestimonialSection = lazy(() => import('/sections/TestimonialSection'));
+const HeaderSection = lazy(() => import('/sections/HeaderSection'));
 
 export default function TopPage() {
 
   return (
     <>
       <div id="hero" className="hero_area">
-        <HeaderSection isTopPage={true} />
-        <SliderSection />
+        <Suspense>
+          <HeaderSection isTopPage={true} />
+        </Suspense>
+        <Suspense>
+          <SliderSection />
+        </Suspense>
       </div>
       <ProductSection layoutPadding={true} isTopPage={true} />
       <AboutSection isTopPage={true} layoutPadding={true} />
       <AchievementSection layoutPadding={true} isTopPage={true} />
-      <TestimonialSection layoutPadding={true} />
+      <Suspense>
+        <TestimonialSection layoutPadding={true} />
+      </Suspense>
       <BackToTopButton />
     </>
   )
