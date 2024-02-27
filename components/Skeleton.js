c8447ba823a6764e31ca4e06c7caa5cc17fcccf6
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2024-02-27 18:41:33 +0700

    refactor(unused): remove unused codes

--- components/Skeleton.js
+++
@@ -1,9 +0,0 @@
-export default function Skeleton({}) {
-  return (
-    <div className="skeleton">
-      <div className="skeleton-image" />
-      <div className="skeleton-text" />
-      <div className="skeleton-text" />
-    </div>
-  );
-};
