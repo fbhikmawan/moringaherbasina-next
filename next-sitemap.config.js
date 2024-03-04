630606e797d99712771a7b9aae345826dcff5584
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2024-03-04 18:52:04 +0700

    refactor[live]: set tge correct live domain in sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://nurhadi.asaidgroup.com',
+  siteUrl: 'https://www.moringaherbasina.com',
   generateRobotsTxt: true,
 }
