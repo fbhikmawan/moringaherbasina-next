cac7e7ca0fc9702aadea351d9654730b95069417
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2024-05-29 19:10:10 +0700

    add activities

--- app/(main)/achievements/page.js
+++ app/(main)/achievements/page.js
@@ -42,6 +42,16 @@ export default function AchievementsPage() {
     { url: "assets/images/activities/smkn-ngasem/smkn-ngasem-3.webp", description: "SMKN Ngasem Internship Program" },
     { url: "assets/images/activities/smkn-ngasem/smkn-ngasem-4.webp", description: "SMKN Ngasem Internship Program" },
   ];
+  const actPameranProdukUnggulan = [
+    { url: "assets/images/activities/pameran-produk-unggulan/pameran-produk-unggulan-1.webp", description: "Pameran Produk Unggulan Bojonegoro" },
+    { url: "assets/images/activities/pameran-produk-unggulan/pameran-produk-unggulan-2.webp", description: "Pameran Produk Unggulan Bojonegoro" },
+    { url: "assets/images/activities/pameran-produk-unggulan/pameran-produk-unggulan-3.webp", description: "Pameran Produk Unggulan Bojonegoro" },
+    { url: "assets/images/activities/pameran-produk-unggulan/pameran-produk-unggulan-4.webp", description: "Pameran Produk Unggulan Bojonegoro" },
+    { url: "assets/images/activities/pameran-produk-unggulan/pameran-produk-unggulan-5.webp", description: "Pameran Produk Unggulan Bojonegoro" },
+    { url: "assets/images/activities/pameran-produk-unggulan/pameran-produk-unggulan-6.webp", description: "Pameran Produk Unggulan Bojonegoro" },
+    { url: "assets/images/activities/pameran-produk-unggulan/pameran-produk-unggulan-7.webp", description: "Pameran Produk Unggulan Bojonegoro" },
+    { url: "assets/images/activities/pameran-produk-unggulan/pameran-produk-unggulan-8.webp", description: "Pameran Produk Unggulan Bojonegoro" },
+  ];
   const workshops = [
     { url: "/assets/images/workshops/workshop-pbp.webp", description: "Magang Budidaya dan Pengolahan Tanaman Kelor Moringa Organik Indonesia" },
     { url: "/assets/images/workshops/workshop-penyuluhan-pirt.webp", description: "Penyuluhan Keamanan Pangan" },
@@ -64,30 +74,30 @@ export default function AchievementsPage() {
             <br className="pc-only" /> Our certifications demonstrate our commitment to excellence, quality and professionalism.
           </p>
         </div>
-        <div class="certifications row">
-          <div class="certification">
-            <h4 class="certification-name">Sertifikat Halal</h4>
-            <p class="certification-details">Issuer: Badan Penyelenggara Jaminan Produk Halal RI</p>
-            <p class="certification-details">Regist Number: ID35110000145391021</p>
-            <p class="certification-details">Date Issued: 19 November 2021</p>
+        <div className="certifications row">
+          <div className="certification">
+            <h4 className="certification-name">Sertifikat Halal</h4>
+            <p className="certification-details">Issuer: Badan Penyelenggara Jaminan Produk Halal RI</p>
+            <p className="certification-details">Regist Number: ID35110000145391021</p>
+            <p className="certification-details">Date Issued: 19 November 2021</p>
           </div>
-          <div class="certification">
-            <h4 class="certification-name">Perizinan Berusaha Berbasis Risiko</h4>
-            <p class="certification-details">Issuer: Badan Koordinasi Penanaman Modal RI</p>
-            <p class="certification-details">Regist Number: 0220007410246</p>
-            <p class="certification-details">Date Issued: 4 April 2020</p>
+          <div className="certification">
+            <h4 className="certification-name">Perizinan Berusaha Berbasis Risiko</h4>
+            <p className="certification-details">Issuer: Badan Koordinasi Penanaman Modal RI</p>
+            <p className="certification-details">Regist Number: 0220007410246</p>
+            <p className="certification-details">Date Issued: 4 April 2020</p>
           </div>
-          <div class="certification">
-            <h4 class="certification-name">Sertifikat Produksi Pangan Industri Rumah Tangga (PIRT)</h4>
-            <p class="certification-details">Issuer: Dinas Kesehatan RI</p>
-            <p class="certification-details">Regist Number: 5133522011295-25</p>
-            <p class="certification-details">Date Issued: 3 September 2020</p>
+          <div className="certification">
+            <h4 className="certification-name">Sertifikat Produksi Pangan Industri Rumah Tangga (PIRT)</h4>
+            <p className="certification-details">Issuer: Dinas Kesehatan RI</p>
+            <p className="certification-details">Regist Number: 5133522011295-25</p>
+            <p className="certification-details">Date Issued: 3 September 2020</p>
           </div>
-          <div class="certification">
-            <h4 class="certification-name">Sertifikat Merek</h4>
-            <p class="certification-details">Issuer: KemenKumHAM RI</p>
-            <p class="certification-details">Regist Number: IDM001019494</p>
-            <p class="certification-details">Date Issued: 4 Oktober 2021</p>
+          <div className="certification">
+            <h4 className="certification-name">Sertifikat Merek</h4>
+            <p className="certification-details">Issuer: KemenKumHAM RI</p>
+            <p className="certification-details">Regist Number: IDM001019494</p>
+            <p className="certification-details">Date Issued: 4 Oktober 2021</p>
           </div>
         </div>
         <div className="heading_container heading_center pt-4 pt-md-5">
@@ -98,6 +108,17 @@ export default function AchievementsPage() {
           </p>
         </div>
         <div className="row justify-content-center">
+          <div className="col-md-6 my-3">
+            <div className="heading_container heading_center">
+              <h3>Bojonegoro Featured Product Exhibition</h3>
+              <p>
+              At the Bojonegoro Featured Product Exhibition, <br/>our Moringa Herbasina products stood out
+              </p>
+            </div>
+            <div className="row">
+              <PhotoGallery images={actPameranProdukUnggulan} />
+            </div>
+          </div>
           <div className="col-md-6 my-3">
             <div className="heading_container heading_center">
               <h3>Dharma Wanita</h3>
