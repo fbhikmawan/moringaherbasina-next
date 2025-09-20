3ea7e2931d5c84e5150b84dc82500b6c2b2f5f49
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2025-09-20 14:42:45 +0700

    next | loosen CSP measurements

--- next/next.config.ts
+++ next/next.config.ts
@@ -31,10 +31,6 @@ const nextConfig: NextConfig = {
             value: '0'
           },
           {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://embed.tawk.to https://www.clarity.ms https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https: https://www.google-analytics.com https://va.tawk.to; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://va.tawk.to wss://va.tawk.to https://www.clarity.ms; frame-src https://embed.tawk.to; frame-ancestors 'none';"
-          },
-          {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin'
           }
